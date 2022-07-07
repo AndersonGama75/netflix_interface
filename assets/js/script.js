a730cd1e2a493b28208a5080a9c4caf24d0ec514
AndersonGama <sagaliber@gmail.com>
2022-07-07 20:39:03 -0300

    Refinamento da interface

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+/*
+var filmesCarrossel = document.getElementsByClassName('item');
+filmesCarrossel[0].addEventListener("mouseover", ampliarCartaz);
+filmesCarrossel[0].addEventListener("mouseout", restaurarCartaz);
+console.log(filmesCarrossel);
+
+function ampliarCartaz(){
+
+    for(let i = 0; i < filmesCarrossel.length; i++){
+        filmesCarrossel[i].style.width = "125%";
+        console.log(`teste ${i}`);
+    }
+}
+
+function restaurarCartaz(){
+    
+    for(let i = 0; i < filmesCarrossel.length; i++){
+        filmesCarrossel[i].style.width = "100%";
+    }
+} */
